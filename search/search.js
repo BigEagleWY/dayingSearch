d4fa7211a2c7d847a79c1d219486fc597a4c3a9a
wangyang <wangyang@vmobel.com>
2018-09-04 13:29:44 +0800

    config

--- search/search.js
+++ search/search.js
@@ -1,14 +1,8 @@
 var engine = require("../engine/engine");
 var filter = require("../filter/filter");
-function search() {
-    filter.key = "node";
-    engine.selectText = "ul.entry-list li.item a.title";
-    engine.search("https://juejin.im/search?query=node",filter.filterByKey,{
-        interval:1,
-        from:1,
-        to:5,
-        urlPro:"https://juejin.im/search?query=node?t=",
-        urlAfter:"&x=xxx"
-    });
+function search(url,keyword,selectText,config) {
+    filter.key = keyword;
+    engine.selectText = selectText;
+    engine.search(url,filter.filterByKey,config);
 }
-search();
+module.exports = search;
